d150b168e2c610c5a92acac02cd4e17b19c19c0b
Jia\Haha <jiajiajiajia10003@163.com>
2019-12-20 17:13:50 +0800

    + ： 增加核心客户区域功能。目前，使用水平布局和垂直布局，将该区域分为2行，共3个部分。

--- QT_Netease_Music/mainwindow.cpp
+++ QT_Netease_Music/mainwindow.cpp
@@ -9,6 +9,12 @@
 
 #include <QDockWidget>
 
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+
+#include <QTreeWidget>
+#include <QTreeWidgetItem>
+
 #include <QtDebug>
 #include <QAction>
 
@@ -66,18 +72,72 @@ MainWindow::MainWindow(QWidget *parent)
     status_label3->setText("part 3");
     sBar->addPermanentWidget(status_label3);
 
-    // 浮动窗口
-    QDockWidget* dock = new QDockWidget("dock1", this);
-    addDockWidget(Qt::RightDockWidgetArea, dock);
-    // 浮动窗口，添加各种控件
-    QTextEdit* dock_edit = new QTextEdit(this);
-    dock->setWidget(dock_edit);
+//    // 浮动窗口----暂时关闭右侧的浮动窗口
+//    QDockWidget* dock = new QDockWidget("dock1", this);
+//    addDockWidget(Qt::RightDockWidgetArea, dock);
+//    // 浮动窗口，添加各种控件
+//    QTextEdit* dock_edit = new QTextEdit(this);
+//    dock->setWidget(dock_edit);
 
     QDockWidget* dock2 = new QDockWidget("dock2", this);
     addDockWidget(Qt::BottomDockWidgetArea, dock2);
     // 浮动窗口，添加各种控件
     QTextEdit* dock_edit2 = new QTextEdit(this);
     dock2->setWidget(dock_edit2);
+
+    // 将客户区域，简单的拆分为2行，共3个区域
+    QVBoxLayout* vLayout = new QVBoxLayout(this);
+    QHBoxLayout* hLayout = new QHBoxLayout(this);
+
+    // TreeWidget控件
+    QTreeWidget* treeWidget = new QTreeWidget(this);
+    //第一组
+    QTreeWidgetItem *group1=new QTreeWidgetItem();
+    group1->setText(0,"group1");
+    group1->setFlags(Qt::ItemIsUserCheckable|Qt::ItemIsEnabled|Qt::ItemIsSelectable);
+    group1->setCheckState(0,Qt::Unchecked);
+    QTreeWidgetItem *item11=new QTreeWidgetItem(group1);
+    item11->setText(0,"item11");
+    item11->setFlags(Qt::ItemIsUserCheckable|Qt::ItemIsEnabled|Qt::ItemIsSelectable);
+    item11->setCheckState(0,Qt::Unchecked);
+    QTreeWidgetItem *item12=new QTreeWidgetItem(group1);
+    item12->setText(0,"item12");
+    item12->setFlags(Qt::ItemIsUserCheckable|Qt::ItemIsEnabled|Qt::ItemIsSelectable);
+    item12->setCheckState(0,Qt::Unchecked);
+    QTreeWidgetItem *item13=new QTreeWidgetItem(group1);
+    item13->setText(0,"item13");
+    item13->setFlags(Qt::ItemIsUserCheckable|Qt::ItemIsEnabled|Qt::ItemIsSelectable);
+    item13->setCheckState(0,Qt::Unchecked);
+
+    // 添加tree的顶层节点
+    treeWidget->insertTopLevelItem(0, group1);
+    // 节点始终展开某个节点
+    treeWidget->setItemsExpandable(false);
+    treeWidget->expandItem(group1);
+
+    // 右侧区域
+    QTextEdit *textedit = new QTextEdit(this);
+    // 底部区域
+    QTextEdit *textedit2 = new QTextEdit(this);
+    //
+    hLayout->addWidget(treeWidget);
+    hLayout->addWidget(textedit);
+
+    // 给水平布局器，增加一个widget封装
+    QWidget *nan_hlayout_widget = new QWidget() ;
+    nan_hlayout_widget->setLayout(hLayout);
+
+    vLayout->addWidget(nan_hlayout_widget);
+    vLayout->addWidget(textedit2);
+
+    // 给垂直布局器，增加一个widget封装
+    QWidget *nan_vlayout_widget = new QWidget() ;
+    nan_vlayout_widget->setLayout(vLayout);
+    nan_vlayout_widget->setWindowTitle("centralwidget");
+
+    // 核心客户区域
+    setCentralWidget(nan_vlayout_widget);
+
 }
 
 MainWindow::~MainWindow()
