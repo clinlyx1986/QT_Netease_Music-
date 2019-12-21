045019113c2859ebb1d91a6dd99c10fb32848b7e
Jia\Haha <jiajiajiajia10003@163.com>
2019-12-21 18:26:12 +0800

    + ： 调整右侧区域的pushbutton控件的hover的背景色，以及2个中文乱码问题。

--- QT_Netease_Music/MiddleWidgetLeftList.cpp
+++ QT_Netease_Music/MiddleWidgetLeftList.cpp
@@ -1,4 +1,4 @@
-#include "MiddleWidgetLeftList.h"
+﻿#include "MiddleWidgetLeftList.h"
 
 MiddleWidgetLeftList::MiddleWidgetLeftList(QWidget *parent) : QFrame(parent)
 {
@@ -14,7 +14,7 @@ MiddleWidgetLeftList::MiddleWidgetLeftList(QWidget *parent) : QFrame(parent)
 void MiddleWidgetLeftList::initTopWidget()
 {
     recommendLabel = new QLabel(this);
-    recommendLabel->setText("推荐");
+    recommendLabel->setText(QStringLiteral("推荐"));
     recommendLabel->setGeometry(0,0,200,30);
     recommendLabel->setStyleSheet("QLabel{color:rgb(92,92,92);border-width: 0 1 0 1;}");
 
@@ -23,33 +23,33 @@ void MiddleWidgetLeftList::initTopWidget()
     findMusicBtn->setCheckable(true);
     findMusicBtn->setChecked(true);
     findMusicBtn->setStyleSheet("QPushButton{background:transparent;border-image:url(:/images/LeftList/findMusic.png)}\
-                                 QPushButton::hover{color:rgb(0,0,0)}\
+                                 QPushButton::hover{background:rgb(221,221,221)}\
                                  QPushButton::checked{background:rgb(230,231,234)}");
 
     privateFMBtn = new QPushButton(this);
     privateFMBtn->setGeometry(0,60,200,30);
     privateFMBtn->setCheckable(true);
     privateFMBtn->setStyleSheet("QPushButton{background:transparent;border-image:url(:/images/LeftList/fm.png)}\
-                                 QPushButton::hover{color:rgb(0,0,0)}\
+                                 QPushButton::hover{background:rgb(221,221,221)}\
                                  QPushButton::checked{background:rgb(230,231,234)}");
 
     mvBtn = new QPushButton(this);
     mvBtn->setGeometry(0,90,200,30);
     mvBtn->setCheckable(true);
     mvBtn->setStyleSheet("QPushButton{background:transparent;border-image:url(:/images/LeftList/mv.png)}\
-                          QPushButton::hover{color:rgb(0,0,0)}\
+                          QPushButton::hover{background:rgb(221,221,221)}\
                           QPushButton::checked{background:rgb(230,231,234);}");
 
     friendBtn = new QPushButton(this);
     friendBtn->setGeometry(0,120,200,30);
     friendBtn->setCheckable(true);
     friendBtn->setStyleSheet("QPushButton{background:transparent;border-image:url(:/images/LeftList/friend.png)}\
-                              QPushButton::hover{color:rgb(0,0,0)}\
+                              QPushButton::hover{background:rgb(221,221,221)}\
                               QPushButton::checked{background:rgb(230,231,234);}");
 
 
     myMusicLabel = new QLabel(this);
-    myMusicLabel->setText("我的音乐");
+    myMusicLabel->setText(QStringLiteral("我的音乐"));
     myMusicLabel->setGeometry(0,180,200,30);
     myMusicLabel->setStyleSheet("QLabel{color:rgb(92,92,92);border-width: 0 1 0 1;}");
 
@@ -57,28 +57,28 @@ void MiddleWidgetLeftList::initTopWidget()
     localMusicBtn->setGeometry(0,210,200,30);
     localMusicBtn->setCheckable(true);
     localMusicBtn->setStyleSheet("QPushButton{background:transparent;border-image:url(:/images/LeftList/local.png)}\
-                                  QPushButton::hover{color:rgb(0,0,0)}\
+                                  QPushButton::hover{background:rgb(221,221,221)}\
                                   QPushButton::checked{background:rgb(230,231,234)}");
 
     downManageBtn = new QPushButton(this);
     downManageBtn->setGeometry(0,240,200,30);
     downManageBtn->setCheckable(true);
     downManageBtn->setStyleSheet("QPushButton{background:transparent;border-image:url(:/images/LeftList/download.png)}\
-                                  QPushButton::hover{color:rgb(0,0,0)}\
+                                  QPushButton::hover{background:rgb(221,221,221)}\
                                   QPushButton::checked{background:rgb(230,231,234)}");
 
     myMusicBoxBtn = new QPushButton(this);
     myMusicBoxBtn->setGeometry(0,270,200,30);
     myMusicBoxBtn->setCheckable(true);
     myMusicBoxBtn->setStyleSheet("QPushButton{background:transparent;border-image:url(:/images/LeftList/yun.png)}\
-                                  QPushButton::hover{color:rgb(0,0,0)}\
+                                  QPushButton::hover{background:rgb(221,221,221)}\
                                   QPushButton::checked{background:rgb(230,231,234)}");
 
     mySingerBtn = new QPushButton(this);
     mySingerBtn->setGeometry(0,300,200,30);
     mySingerBtn->setCheckable(true);
     mySingerBtn->setStyleSheet("QPushButton{background:transparent;border-image:url(:/images/LeftList/single.png)}\
-                                QPushButton::hover{color:rgb(0,0,0)}\
+                                QPushButton::hover{background:rgb(221,221,221)}\
                                 QPushButton::checked{background:rgb(230,231,234)}");
 
 
